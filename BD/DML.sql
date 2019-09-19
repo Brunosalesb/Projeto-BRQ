14b9cabfe423c46bba5568908261dda4576da3f2
Bruno Sales Bernardes <51418943800@info.local>
2019-09-19 14:20:43 -0300

    DDL e DML

---
+++ BD/DML.sql
@@ -0,0 +1,33 @@
+use brq_senai;
+
+select * from tipoSkill
+insert into tipoSkill
+values ('Skill Técnica'),('Skill Comportamental')
+
+select * from skill
+insert into skill
+values ('Java',1)
+
+select * from tipoExperiencia
+insert into tipoExperiencia
+values ('Interno'),('Externo'),('Formação')
+
+select * from pessoa
+insert into pessoa
+values ('12345','Natália Marina Luna Peixoto','530.113.276-88','26.530.986-4','1979-07-12','(83)99396-6636','nataliapeixoto@gmail.com','58705-560','Rua Robério Vieira de Medeiros','589',null,'Jardim Magnólia','Patos','PB')
+
+select * from experiencia
+insert into experiencia
+values ('Desenvolvedor Java','BRQ Digital Solutions','Atuação na manutenção de funcionalidades existentes e no desenvolvimento de novas, utilizando 
+Java','2019-04-17','2019-08-22',2,2)
+
+select * from skills_pessoa
+insert into skills_pessoa
+values(2,1)
+
+select * from pessoa
+select * from skills_pessoa
+select * from skill
+select * from tipoSkill
+select * from experiencia
+select * from tipoExperiencia
