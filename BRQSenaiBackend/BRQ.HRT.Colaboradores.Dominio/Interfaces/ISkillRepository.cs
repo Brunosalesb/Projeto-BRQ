c0cad50cf34831009785ee2d0085353f25fd27e4
Bruno Sales Bernardes <51418943800@info.local>
2019-10-11 17:05:14 -0300

    apagando

--- BRQSenaiBackend/BRQ.HRT.Colaboradores.Dominio/Interfaces/ISkillRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BRQ.HRT.Colaboradores.Dominio.Entidades;
-
-
-namespace BRQ.HRT.Colaboradores.Dominio.Interfaces
-{
-    public interface ISkillRepository : IBaseRepository<Skill>
-    {
-    }
-}
